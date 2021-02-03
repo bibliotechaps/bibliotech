f99486a4e6b605c8dda4abb0985efc4c1d552d3c
Allan <allan.sousatr@hotmail.com>
2021-02-03 18:49:36 -0300

    Allan - Consulta admin

--- perfil-admin.php
+++ perfil-admin.php
@@ -1,5 +1,10 @@
 <?php
 session_start();
+include("conexao.php");
+
+$query = "select * from admin where login = '{$_SESSION['login']}'";
+$result = mysqli_query ($conn, $query);
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -64,56 +69,84 @@ session_start();
             </div>
 
             <div class="row justify-content-around p-3 w-100 m-0">
+                <?php while($dado = $result->fetch_array()){ ?>
 
                 <div class="form-group  col-4">
                     <label style="margin: auto;" for="nome_cad">Nome:</label><br>
-                    <input name="nome_cad" id="nome_cad" class="form-control" type="text" placeholder="Seu nome" autofocus required readonly>
+                    <input name="nome_cad" id="nome_cad" class="form-control" type="text" value="<?php
+                                                                                                    echo $dado['nome'];
+                                                                                                    ?>" readonly>
                 </div>
 
                 <div class="form-group  col-4">
                     <label style="margin: auto;" for="login_cad">Login:</label><br>
-                    <input name="login_cad" id="login_cad" class="form-control" type="text" placeholder="Seu Login" required readonly>
+                    <input name="login_cad" id="login_cad" class="form-control" type="text" value="<?php
+                                                                                                    echo $dado['login'];
+                                                                                                    ?>" readonly>
                 </div>
 
                 <div class="form-group  col-4">
                     <label style="margin: auto;" for="email_cad">Email:</label><br>
-                    <input name="email_cad" id="email_cad" class="form-control" type="text" placeholder="Seu Email" required readonly>
+                    <input name="email_cad" id="email_cad" class="form-control" type="text" value="<?php
+                                                                                                    echo $dado['email'];
+                                                                                                    ?>" readonly>
                 </div>
 
                 <div class="form-group  col-4">
                     <label style="margin: auto;" for="datanasc_cad">Data de Nascimento:</label><br>
-                    <input name="datanasc_cad" id="datanasc_cad" class="form-control" type="date" required readonly>
+                    <input name="datanasc_cad" id="datanasc_cad" class="form-control" type="date" value="<?php
+                                                                                                            echo $dado['dataNasc'];
+                                                                                                            ?>" readonly>
                 </div>
 
                 <div class="form-group  col-4">
                     <label style="margin: auto;" for="cpf_cad">CPF:</label><br>
-                    <input name="cpf_cad" id="cpf_cad" class="form-control" type="number" placeholder="Seu CPF" required minlength="11" maxlength="11" readonly>
+                    <input name="cpf_cad" id="cpf_cad" class="form-control" type="text" value="<?php
+                                                                                                    echo $dado['cpf'];
+                                                                                                    ?>" readonly>
                 </div>
                 <div class="form-group  col-4">
                     <label style="margin: auto;" for="tel_cad">Telefone:</label><br>
-                    <input name="tel_cad" id="tel_cad" class="form-control" type="tel" placeholder="Seu Telefone" minlength="11" maxlength="11" readonly>
-                </div>
-                <div class="form-group  col-3">
-                    <label style="margin: auto;" for="end_cad">Endereço:</label><br>
-                    <input name="end_cad" id="end_cad" class="form-control" type="text" placeholder="Seu Endereço" required readonly>
+                    <input name="tel_cad" id="tel_cad" class="form-control" type="tel" value="<?php
+                                                                                                echo $dado['telefone'];
+                                                                                                ?>" readonly>
                 </div>
 
                 <div class="form-group  col-3">
                     <label style="margin: auto;" for="cep_cad">CEP:</label><br>
-                    <input name="cep_cad" id="cep_cad" class="form-control" type="number" placeholder="Seu CEP" required minlength="8" maxlength="8" readonly>
+                    <input name="cep_cad" id="cep_cad" class="form-control" type="text" value="<?php
+                                                                                                    echo $dado['cep'];
+                                                                                                    ?>" readonly>
                 </div>
 
                 <div class="form-group  col-3">
-                    <label style="margin: auto;" for="bairro_cad">Bairro:</label><br>
-                    <input name="bairro_cad" id="bairro_cad" class="form-control" type="text" placeholder="Seu Bairro" required readonly>
+                    <label style="margin: auto;" for="estado_cad">Estado:</label><br>
+                    <input name="estado_cad" id="estado_cad" class="form-control" style=" text-transform: uppercase" type="text" value="<?php
+                                                                                                        echo $dado['estado'];
+                                                                                                        ?>" readonly>
                 </div>
 
                 <div class="form-group  col-3">
                     <label style="margin: auto;" for="cidade_cad">Cidade:</label><br>
-                    <input name="cidade_cad" id="cidade_cad" class="form-control" type="text" placeholder="Sua Cidade" required readonly>
+                    <input name="cidade_cad" id="cidade_cad" class="form-control" type="text" value="<?php
+                                                                                                        echo $dado['cidade'];
+                                                                                                        ?>" readonly>
+                </div>
+                <div class="form-group  col-3">
+                    <label style="margin: auto;" for="bairro_cad">Bairro:</label><br>
+                    <input name="bairro_cad" id="bairro_cad" class="form-control" type="text" value="<?php
+                                                                                                        echo $dado['bairro'];
+                                                                                                        ?>" readonly>
                 </div>
 
 
+                <div class="form-group  col-6">
+                    <label style="margin: auto;" for="end_cad">Endereço:</label><br>
+                    <input name="end_cad" id="end_cad" class="form-control" type="text" value="<?php
+                                                                                                echo $dado['endereco'];
+                                                                                                ?>" readonly>
+                </div>
+            <?php } ?>
 
             </div>
 
