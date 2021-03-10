8644f991b023621bfe260e5d0f455e404bb5cf90
PHNRC <phnrc@hotmail.com>
2021-03-10 10:44:05 -0300

    Rumano

--- efetuar-emprestimo.php
+++ efetuar-emprestimo.php
@@ -8,7 +8,7 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Adicionar Livro</title>
+    <title>Efetuar Empréstimo</title>
     <link rel="stylesheet" href="css/arquivo1.css">
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
