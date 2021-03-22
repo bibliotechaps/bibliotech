46f03ef5fd52cfec99af8ac179c82678af2d9bd4
PHNRC <phnrc@hotmail.com>
2021-03-22 19:21:29 -0300

    pesquisar reserva - pedro

--- pesquisa-emprestimo.php
+++ pesquisa-emprestimo.php
@@ -1,81 +1,47 @@
-<?php
-    session_start();
-    include_once('conexao.php');
-    $pesquisa = $conn->real_escape_string($_POST['pesquisa']);
+    <table class="table table-striped table-hover">
+        <tr>
+            <td><b>ID</b></td>
+            <td><b>Data Inicial</b></td>
+            <td><b>Data Final</b></td>
+            <td><b>Administrador</b></td>
+            <td><b>Aluno</b></td>
+            <td><b>Livro</b></td>
+        </tr>
+        <tr>
+            <?php
+            session_start();
+            include_once('conexao.php');
+            $pesquisa = $conn->real_escape_string($_POST['pesquisa']);
 
-    //criar o comando sql para a busca na base de dados
-    $sql =  "SELECT * 
-                FROM emprestimo
-                where aluno_id like '%$pesquisa%' 
-                or admin_id like '%$pesquisa%'
-                order by aluno_id";
+            $sql = "SELECT emprestimo.id, emprestimo.dataInicial, emprestimo.dataFinal, admin.nome, aluno.nome, livro.titulo
+                FROM aluno 
+                JOIN emprestimo ON emprestimo.aluno_id = aluno.id
+                JOIN admin ON admin.id = emprestimo.admin_id
+                JOIN livro ON livro.id = emprestimo.livro_id
+                WHERE aluno.nome like '%$pesquisa%'
+                OR livro.titulo like '%$pesquisa%'
+                ORDER BY aluno.nome ASC";
 
-     //executar o comando sql
-     $dadosEmprestimos = $conn->query($sql);
-     if($dadosEmprestimos->num_rows > 0){
-        ?>
-        <table class="table table-striped table-hover">
-                    <tr>
-                        <td><b>ID</b></td>
-                        <td><b>Data Inicial</b></td>
-                        <td><b>Data Final</b></td>
-                        <td><b>Administrador</b></td>
-                        <td><b>Aluno</b></td>
-                        <td><b>Livro</b></td>
-                    </tr>
+            $result = mysqli_query($conn, $sql);
+            $array = [];
 
-                    <?php 
+            if (mysqli_num_rows($result) != 0) {
+                while ($row = mysqli_fetch_array($result)) {
+                    array_push($array, $row);
 
-                        //fetch_assoc() retorna cada linha da matriz
-                        while($exibir = $dadosEmprestimos->fetch_assoc()){ 
-                            ?>
-                            <tr>
-                                <td><?php echo $exibir["id"]?></td>
-                                <td><?php echo $exibir["dataInicial"]?></td>
-                                <td><?php echo $exibir["dataFinal"]?></td>
-                                <?php
-                                //busca os dados do administrador com base no código da tabela de livros
-                                $sqlAdministrador = "SELECT * FROM admin WHERE id = " . $exibir["admin_id"];
-                                $dadosAdministrador = $conn->query($sqlAdministrador);
-                                $administrador = $dadosAdministrador->fetch_assoc();
-                                ?>
-                                <td><?php echo $administrador["nome"]?></td>
-                                <?php
-                                //busca os dados do aluno com base no código da tabela de livros
-                                $sqlAluno = "SELECT * FROM aluno WHERE id = " . $exibir["aluno_id"];
-                                $dadosAluno = $conn->query($sqlAluno);
-                                $aluno = $dadosAluno->fetch_assoc();
-                                ?>
-                                <td><?php echo $aluno["nome"]?></td>
-                                <?php
-                                //busca os dados do livro com base no código da tabela de livros
-                                $sqlLivro = "SELECT * FROM livro WHERE id = " . $exibir["livro_id"];
-                                $dadosLivro = $conn->query($sqlLivro);
-                                $livro = $dadosLivro->fetch_assoc();
-                                ?>
-                                <td><?php echo $livro["titulo"]?></td>
-                            </tr>
-                            <?php
-                        }
+                    echo
+                    '<tr></tr><td> ' . $row[0] . '</td>' .
+                        '<td> ' . $row[1] . '</td>' .
+                        '<td> ' . $row[2] . '</td>' .
+                        '<td> ' . $row[3] . '</td>' .
+                        '<td> ' . $row[4] . '</td>' .
+                        '<td> ' . $row[5] . '</td></tr>';
+                }
+            }
 
-                    ?>
+            ?>
+        </tr>
+    </table>
 
-                </table>
-
-                <br> 
-                <br>
-        
-
-
-     <?php
-         
-     }         
-     else{
-        echo "Nenhum registro retornado! Verifique seus filtros.";
-        ?>
-            <br>
-            <br>
-           
-        <?php
-     }
-?>
+    <br>
+    <br>
